3de8aeb022ee31b3b5020657dbb39a0e3c39268f
pfuerste <philip.fuerste@gmail.com>
2022-10-08 11:56:16 +0200

    started converting old notebook

--- src/data/ingestion.py
+++ src/data/ingestion.py
@@ -6,3 +6,15 @@ from src.data.decorators import ingestion_decorator
 @ingestion_decorator
 def read_csv(path):
     return pd.read_csv(path)
+
+
+@ingestion_decorator
+def delete_broken_rows(df):
+    try:
+        # No sentinel data for this olc_id available
+        df = df[df.olc_id != "5G55HPG4+MM7"]
+    except KeyError or ValueError:
+        print(
+            "Tried to delete olc_id, '5G55HPG4+MM7', did not find it. Passing uncleaned df."
+        )
+    return df
--- src/features/csv_engineering.py
+++ src/features/csv_engineering.py
@@ -1,18 +1,20 @@
 import pandas as pd
 import numpy as np
 
+from src.features.decorators import csv_preprocessing_decorator
 
-# target_and_input_OpenLandMap = target_and_input_OpenLandMap[target_and_input_OpenLandMap.olc_id != "5G55HPG4+MM7"]
 
-
+@csv_preprocessing_decorator
 def drop_high_oc(df):
     return df[df["oc"] < 120]
 
 
+@csv_preprocessing_decorator
 def fill_na(data, fill_with=0):
     return data.fillna(fill_with, inplace=True)
 
 
+@csv_preprocessing_decorator
 def minmax_scale_columns(data: pd.DataFrame):
     mins = data.min(axis=0)
     maxs = data.max(axis=0)
@@ -20,5 +22,6 @@ def minmax_scale_columns(data: pd.DataFrame):
     return data
 
 
+@csv_preprocessing_decorator
 def log_column(data):
     return np.log(data, axis=0)

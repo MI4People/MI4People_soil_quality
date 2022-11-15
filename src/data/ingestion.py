c5ae4817444552ef10f7deccead48318b44ce878
pfuerste <philip.fuerste@gmail.com>
2022-11-15 19:22:09 +0100

    added filter and type hints

--- src/data/ingestion.py
+++ src/data/ingestion.py
@@ -1,15 +1,24 @@
 import pandas as pd
+import intake
 
-from src.data.decorators import ingestion_decorator
+from src.globals import PROJECT_DIR, SRC_DIR, CATALOG
+from src.data.decorators import csv_ingestion_decorator
 
 
-@ingestion_decorator
+@csv_ingestion_decorator
 def read_csv(path):
     return pd.read_csv(path)
 
 
-@ingestion_decorator
-def delete_broken_rows(df):
+@csv_ingestion_decorator
+def read_csv_from_catalog(datasource_name: str):
+    cat = intake.open_catalog(CATALOG)
+    datasource = cat[datasource_name].read()
+    return datasource
+
+
+@csv_ingestion_decorator
+def delete_broken_rows(df: pd.DataFrame):
     try:
         # No sentinel data for this olc_id available
         df = df[df.olc_id != "5G55HPG4+MM7"]
--- src/features/csv_engineering.py
+++ src/features/csv_engineering.py
@@ -1,16 +1,27 @@
 import pandas as pd
 import numpy as np
+from pathlib import Path
 
+from src.globals import PROJECT_DIR, SRC_DIR, CATALOG
 from src.features.decorators import csv_preprocessing_decorator
 
 
 @csv_preprocessing_decorator
-def drop_high_oc(df):
+def filter_corrupted_sites(df):
+    corrupted_sites = pd.read_csv(
+        PROJECT_DIR / "data/raw/RaCA2016_and_ISCND_points.csv"
+    )["olc_id"]
+    df = df[~df["olc_id"].isin(corrupted_sites)]
+    return df
+
+
+@csv_preprocessing_decorator
+def drop_high_oc(df: pd.DataFrame):
     return df[df["oc"] < 120]
 
 
 @csv_preprocessing_decorator
-def fill_na(data, fill_with=0):
+def fill_na(data: pd.DataFrame, fill_with: int = 0):
     return data.fillna(fill_with, inplace=True)
 
 
@@ -23,5 +34,5 @@ def minmax_scale_columns(data: pd.DataFrame):
 
 
 @csv_preprocessing_decorator
-def log_column(data):
+def log_column(data: pd.DataFrame):
     return np.log(data, axis=0)

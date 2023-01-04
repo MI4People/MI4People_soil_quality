12d1f73868fee1902f4edab6a06cb3b66aeee6d3
pfuerste <philip.fuerste@gmail.com>
2023-01-04 18:09:58 +0100

    moved function for dataloader/pipes

---
+++ src/data/bigearthnet_datapipes.py
@@ -0,0 +1,79 @@
+import numpy as np
+import torchdata.datapipes as dp
+import json
+import fsspec
+from PIL import Image
+from src.globals import LABELS_TO_INDS
+from ingestion import combine_and_resize_bands
+from src.features.img_engineering import get_first_n_pcs
+
+
+def is_tif_file(path):
+    if path.endswith(".tif"):
+        return True
+    return False
+
+
+def is_json_file(path):
+    if path.endswith(".json"):
+        return True
+    return False
+
+
+def group_key_by_folder(path_and_stream):
+    return "".join(path_and_stream[0].split("/")[-1].split("_")[:5])
+
+
+def chunk_to_dataloader_dict(chunk):
+    json_files = [path for path in chunk if is_json_file(path)]
+    assert len(json_files) == 1, "Only one label json per image is permitted."
+    image_files = [path for path in chunk if is_tif_file(path)]
+    return {"label": json_files[0], "data": image_files}
+
+
+def read_json_from_path(path):
+    json_content = json.loads(fsspec.open(path, mode="r").open().read())
+    return json_content
+
+
+def read_imgs_from_paths(path_list):
+    image_contents = [
+        np.array(Image.open(fsspec.open(path, mode="rb").open())) for path in path_list
+    ]
+    return image_contents
+
+
+def get_labels(meta_dict):
+    return meta_dict["labels"]
+
+
+def get_label_inds(label_list):
+    return [LABELS_TO_INDS[label] for label in label_list]
+
+
+def pca_on_label_and_data(combined_image):
+    return get_first_n_pcs(combined_image, num_components=3)
+
+
+def get_bigearth_pca_pipe(folders):
+    """Given a list of paths (in a bucket), returns a datapipe with simple 3-component PCA performed on images.
+        May be passed in the contruction of a dataloader in  the dataset argument.
+
+    Args:
+        folders (list): List of folders to process. Each folder must contain 12 tifs, one for each band and one json with metadata.
+
+    Returns:
+        torch.utils.data.datapipes.iter.callable.MapperIterDataPipe: Iterable datapipe yielding dicts with keys "data" and "label".
+            Data is a single numpy array of the 3 first PCs of all bands, label a list of integers from /data/raw/bigearth_labels.json.
+    """
+    img_pipe = dp.iter.IterableWrapper(folders)
+    img_pipe = img_pipe.list_files_by_fsspec()
+    img_pipe = img_pipe.groupby(group_key_fn=group_key_by_folder, group_size=13)
+    img_pipe = img_pipe.map(chunk_to_dataloader_dict)
+    img_pipe = img_pipe.map(read_json_from_path, input_col="label")
+    img_pipe = img_pipe.map(get_labels, input_col="label")
+    img_pipe = img_pipe.map(get_label_inds, input_col="label")
+    img_pipe = img_pipe.map(read_imgs_from_paths, input_col="data")
+    img_pipe = img_pipe.map(combine_and_resize_bands, input_col="data")
+    img_pipe = img_pipe.map(pca_on_label_and_data, input_col="data")
+    return img_pipe
--- src/infrastructure/aws_infrastructure.py
+++ src/infrastructure/aws_infrastructure.py
@@ -2,6 +2,8 @@ import yaml
 import os
 from pathlib import Path
 from src.globals import PROJECT_DIR
+import boto3
+import torchdata.datapipes as dp
 
 
 def get_aws_credentials():
@@ -13,3 +15,36 @@ def get_aws_credentials():
 def set_s3_credentials(aws_credentials):
     os.environ["AWS_ACCESS_KEY_ID"] = aws_credentials["s3"]["public_key"]
     os.environ["AWS_SECRET_ACCESS_KEY"] = aws_credentials["s3"]["secret_key"]
+
+
+def download_from_s3(bucket: str, remote_path: str, local_path: str, s3_client=None):
+    if not s3_client:
+        s3_client = boto3.client("s3")
+    s3_client.download_file(bucket, remote_path, local_path)
+
+
+def split_bucket_from_path(x: str):
+    """Separate bucket name and path in bucket from full string.
+
+    Args:
+        x (str): full string
+
+    Returns:
+        tuple: (bucket_name, path)
+    """
+    return "".join(x.split("/")[2:3]), "/".join(x.split("/")[3:])
+
+
+def get_s3_folder_content(bucket_path=f"s3://mi4people-soil-project/BigEarthNet-v1.0/"):
+    """Gets all top level-elements in buckets path.
+
+    Args:
+        bucket_path (string, optional): Path in bucket to list elements from. Defaults to f"s3://mi4people-soil-project/BigEarthNet-v1.0/".
+
+    Returns:
+        list: list of full paths of all top-level-elements.
+    """
+    top_pipe = dp.iter.IterableWrapper([bucket_path])
+    top_pipe = top_pipe.list_files_by_fsspec()
+    folders = list(top_pipe)
+    return folders

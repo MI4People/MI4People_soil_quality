c4dcaf673015f0828070fe0281f0465aa4cdaede
Philip Fürste <philip.fuerste@gmail.com>
2023-02-13 10:12:12 +0100

    added pickled labels

--- src/globals.py
+++ src/globals.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import json
+import pickle
 
 PROJECT_DIR = Path(__file__).parents[1]
 SRC_DIR = Path(__file__).parents[0]
@@ -8,8 +9,12 @@ CATALOG = Path(__file__).parents[1] / "catalog.yaml"
 
 
 def get_bigearth_labels_from_json():
-    with open(DATA_DIR / "raw/bigearth_labels.json", "r") as label_file:
-        ind_to_labels = json.load(label_file)
+    try:
+        with open(DATA_DIR / "raw/bigearth_labels.json", "r") as label_file:
+            ind_to_labels = json.load(label_file)
+    except FileNotFoundError:
+        with open(DATA_DIR / "raw/ben_labels", "rb") as label_file:
+            ind_to_labels = pickle.load(label_file)
     labels_to_ind = {l: int(i) for (i, l) in ind_to_labels.items()}
     return labels_to_ind
 

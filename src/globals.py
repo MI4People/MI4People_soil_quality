492011798320ab5ba49be48eb4a02895b89906d0
Tanja.Fenn <tanja.fenn@student.kit.edu>
2024-01-17 17:51:32 +0100

    adjust try except block so that there is only warning if labels are not detected

--- src/globals.py
+++ src/globals.py
@@ -1,6 +1,8 @@
 from pathlib import Path
-import json
+from typing import Union
 import pickle
+import warnings
+
 
 PROJECT_DIR = Path(__file__).parents[1]
 SRC_DIR = Path(__file__).parents[0]
@@ -8,15 +10,15 @@ DATA_DIR = PROJECT_DIR / "data"
 CATALOG = Path(__file__).parents[1] / "catalog.yaml"
 
 
-def get_bigearth_labels_from_json():
+def get_bigearth_labels_from_json() -> Union[dict, None]:
+    """Returns all potential labels for BigEarthNet dataset if available"""
     try:
-        with open(DATA_DIR / "raw/bigearth_labels.json", "r") as label_file:
-            ind_to_labels = json.load(label_file)
-    except FileNotFoundError:
-        with open(DATA_DIR / "raw/ben_labels", "rb") as label_file:
+        with open(DATA_DIR / "01_raw/ben_labels.pickle", "rb") as label_file:
             ind_to_labels = pickle.load(label_file)
-    labels_to_ind = {l: int(i) for (i, l) in ind_to_labels.items()}
-    return labels_to_ind
-
+            labels_to_ind = {l: int(i) for (i, l) in ind_to_labels.items()} if ind_to_labels else None
+        return labels_to_ind
+    except FileNotFoundError:
+        warnings.warn("If you want to make use of global variable LABELS_TO_INDS containing all labels for the BigEarthNet (BEN) dataset, you need to execute the notebook `bigearthnet_label_creation.ipnyb` first.")
+        return None
 
 LABELS_TO_INDS = get_bigearth_labels_from_json()
